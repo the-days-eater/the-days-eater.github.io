1baa1ee9306311188fdb4969013bc5a267daac83
the-days-eater <tsukihiguiart@gmail.com>
2022-05-02 11:42:16 -0700

    publish.

---
+++ assets/script-2.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#about-text").delay(1000).fadeTo(1000,1,);
+    $("#refs").delay(1700).fadeTo(1000,1,);
+    $("#back-button").delay(2400).fadeTo(1000,1,);
+});
---
+++ assets/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $("#banner-img").delay(500).fadeTo(1000,1);
+    $("h1").delay(1500).fadeTo(1000,1);
+    $("#frame1").delay(1800).fadeTo(1000,1);
+    $("#frame2").delay(2100).fadeTo(1000,1);
+    $("#h2-1").delay(2000).fadeTo(1000,1);
+    $("#p-1").delay(2400).fadeTo(1000,1);
+});
+
+$(window).on("load",function() {
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + ($(this).innerHeight() * .2);
+      $(".fadein").each(function() {
+        /* Check the location of each desired element */
+        var objectBottom = $(this).offset().top + $(this).outerHeight() /2;
+        
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+          if ($(this).css("opacity")==0) {$(this).fadeTo(1000,1);}
+        } else { //object goes out of view (scrolling up)
+          if ($(this).css("opacity")==1) {$(this).fadeTo(1000,0);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+  });
